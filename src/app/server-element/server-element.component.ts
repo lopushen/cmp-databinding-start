987eb48cc11e73f7dd5d667b3cc23a4c0b950c96
Ievgenii Lopushen <lopushen@gmail.com>
2019-07-19 00:32:04 +0300

    native for shadow dom

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: { type: string, content: string };
